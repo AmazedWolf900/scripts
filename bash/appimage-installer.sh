50b696d5ac072051aff79c6c8569e67787e683cd
Tomáš Weber <59090875+AmazedWolf900@users.noreply.github.com>
2021-02-12 10:07:56 +0100

    Update appimage-installer.sh

--- bash/appimage-installer.sh
+++ bash/appimage-installer.sh
@@ -2,26 +2,27 @@
 
 program() {
 	echo 'Enter the name you want the application to have in the application list.'
-	echo 'Program file and icon file must have the same name as the entered one.'
 	read app_name
+	echo 'Enter AppImage and Png file name.'
+	read file_name
 
 	#copy program and icon files to the /usr/bin directory
-	mkdir /usr/bin/$app_name
-	cp $app_name.png /usr/bin/$app_name/$app_name.png
-	cp $app_name.AppImage /usr/bin/$app_name/$app_name.AppImage
-	chmod +x /usr/bin/$app_name/$app_name.AppImage
+	mkdir /usr/bin/$file_name
+	cp $file_name.png /usr/bin/$file_name/$file_name.png
+	cp $file_name.AppImage /usr/bin/$file_name/$file_name.AppImage
+	chmod +x /usr/bin/$file_name/$file_name.AppImage
 
 	#create desktop entry in /usr/share/applications
-	touch /usr/share/applications/$app_name.desktop
+	touch /usr/share/applications/$file_name.desktop
 
-	echo "[Desktop Entry]" >> /usr/share/applications/$app_name.desktop
-	echo "Encoding=UTF-8" >> /usr/share/applications/$app_name.desktop
-	echo "Version=1.0" >> /usr/share/applications/$app_name.desktop
-	echo "Type=Application" >> /usr/share/applications/$app_name.desktop
-	echo "Terminal=false" >> /usr/share/applications/$app_name.desktop
-	echo "Exec=/usr/bin/$app_name/$app_name.AppImage" >> /usr/share/applications/$app_name.desktop
-	echo "Name=$app_name" >> /usr/share/applications/$app_name.desktop
-	echo "Icon=/usr/bin/$app_name/$app_name.png" >> /usr/share/applications/$app_name.desktop
+	echo "[Desktop Entry]" >> /usr/share/applications/$file_name.desktop
+	echo "Encoding=UTF-8" >> /usr/share/applications/$file_name.desktop
+	echo "Version=1.0" >> /usr/share/applications/$file_name.desktop
+	echo "Type=Application" >> /usr/share/applications/$file_name.desktop
+	echo "Terminal=false" >> /usr/share/applications/$file_name.desktop
+	echo "Exec=/usr/bin/$file_name/$file_name.AppImage" >> /usr/share/applications/$file_name.desktop
+	echo "Name=$app_name" >> /usr/share/applications/$file_name.desktop
+	echo "Icon=/usr/bin/$file_name/$file_name.png" >> /usr/share/applications/$file_name.desktop
 
 }
 
